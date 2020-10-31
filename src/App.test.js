616b063de6c3c71aca5cb34c0aff4a8aa6806125
Oscar Hoang <oscar.hoang@gmail.com>
2020-10-31 14:45:47 -0600

    refactor: removed boilerplate code

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders scoreboard react text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/Scoreboard/i);
+  expect(textElement).toBeInTheDocument();
 });
